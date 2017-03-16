d4f80fb5d2b0165636de34fef41ad266bde447e6
Darren Lahr <darrenlahr@gmail.com>
2017-03-16 01:13:59 +0000

    Test Azure

--- corePHP/functions.php
+++ corePHP/functions.php
@@ -129,6 +129,48 @@ function logMeIn() {
 
 function connectToDatabase(){
 
+
+
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
+
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+
+    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    //echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    //echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+    if (mysqli_connect_errno())
+    {
+        die("Failed to connect to MySQL: " . mysqli_connect_error()) ;
+    } else{
+        return $link;
+
+    }
+
+
+
+    /*
+
     if(!defined('SERVERNAME')){
 
 
@@ -152,10 +194,10 @@ function connectToDatabase(){
 
     */
 
-    }
-    $con = mysqli_connect(SERVERNAME,USERNAME,SERVERPASSWORD,DATABASENAME);
+    //}
+    //$con = mysqli_connect(SERVERNAME,USERNAME,SERVERPASSWORD,DATABASENAME);
 
-// Check connection
+ /*
     if (mysqli_connect_errno())
     {
         die("Failed to connect to MySQL: " . mysqli_connect_error()) ;
@@ -163,6 +205,7 @@ function connectToDatabase(){
         return $con;
 
     }
+    */
 
 }
 
