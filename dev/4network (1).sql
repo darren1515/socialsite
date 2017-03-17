a45986f15017791209b77289f24abfa6ccff3b9a
Darren Lahr <darrenlahr@gmail.com>
2017-03-17 16:37:36 +0000

    Latest database, works with azure php 7

--- dev/4network (1).sql
+++ dev/4network (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 17, 2017 at 05:29 PM
+-- Generation Time: Mar 17, 2017 at 05:36 PM
 -- Server version: 5.6.34
 -- PHP Version: 7.1.0
 
